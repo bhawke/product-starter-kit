1a3dd1f54301fc9c8b18373ad7da1baa36547b64
Rob Beers <rbeers@blackhawkenterprise.com>
2015-06-28 00:21:28 -0400

    added animations

--- app/scripts/template-2/app.js
+++ app/scripts/template-2/app.js
@@ -1,27 +1,28 @@
 
 (function(document) {
   'use strict';
+  var app = document.querySelector('#app');
 
   // custom transformation: scale header's title
-  var title = document.querySelector('.page-title');
-  var siteTitle = document.querySelector('.site-title');
+  //var title = document.querySelector('.page-title');
+  //var siteTitle = document.querySelector('.site-title');
   addEventListener('paper-header-transform', function(e) {
     var d = e.detail;
     var m = d.height - d.condensedHeight;
     var scale = Math.max(0.75, (m - d.y) / (m / 0.25)  + 0.75);
 
     if (scale < 1) {
-      title.classList.add('condensed');
-      siteTitle.classList.add('condensed');
+      app.$.pageTitle.classList.add('condensed');
+      app.$.siteTitle.classList.add('condensed');
     } else {
-      title.classList.remove('condensed');
-      siteTitle.classList.remove('condensed');
+      app.$.pageTitle.classList.remove('condensed');
+      app.$.siteTitle.classList.remove('condensed');
     }
 
     if (scale < .9) {
-      title.classList.add('hidden');
+      app.$.pageTitle.classList.add('hidden');
     } else {
-      title.classList.remove('hidden');
+      app.$.pageTitle.classList.remove('hidden');
     }
 
     //Polymer.Base.transform('scale(' + scale + ') translateZ(0)', title);
